3e68f683b85b971580ae6c76374b4f7075090b10
iamsubho76 <iamsubho76@gmail.com>
2017-11-15 21:17:50 +0530

    Zuul Server has been added

---
+++ Itemsharing.Zuulserver/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting zuul server "
+echo "********************************************************"
+java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/zuulservice/@project.build.finalName@.jar
