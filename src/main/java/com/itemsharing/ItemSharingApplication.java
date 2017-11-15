83e416c4778d09c759dd1455d8de8b0283727a0d
iamsubho76 <iamsubho76@gmail.com>
2017-11-15 17:23:44 +0530

    1st Commit

---
+++ src/main/java/com/itemsharing/ItemSharingApplication.java
@@ -0,0 +1,12 @@
+package com.itemsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemSharingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/org/microservices/ItemSharingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.org.microservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemSharingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
